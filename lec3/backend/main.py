655495d1b7e81be26ae4a711d5e610b257944b65
Nat Dilokthanakul <nutbalm@gmail.com>
2025-12-09 12:18:46 +0700

    fix: add reload when running fastapi

--- lec3/backend/main.py
+++ lec3/backend/main.py
@@ -79,4 +79,4 @@ async def create_item(item: ItemCreate, db: AsyncSession = Depends(get_db)):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
